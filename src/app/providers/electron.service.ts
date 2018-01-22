fefac894d93e6769e4635a998e9de42cc6d2551b
Gharbi Dali <shanks.dali@gmail.com>
2018-01-22 16:07:41 +0100

    Update electron.service.ts

--- src/app/providers/electron.service.ts
+++ src/app/providers/electron.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 // If you import a module but never use any of the imported values other than as TypeScript types,
 // the resulting javascript file will look as if you never imported the module at all.
-import { ipcRenderer } from 'electron';
+import { ipcRenderer, app, remote } from 'electron';
 import * as childProcess from 'child_process';
 
 @Injectable()
@@ -10,12 +10,15 @@ export class ElectronService {
 
   ipcRenderer: typeof ipcRenderer;
   childProcess: typeof childProcess;
-
+  app: typeof app;
+remote: typeof remote;
   constructor() {
     // Conditional imports
     if (this.isElectron()) {
       this.ipcRenderer = window.require('electron').ipcRenderer;
       this.childProcess = window.require('child_process');
+      this.app = window.require('electron').app;
+      this.remote = window.require('electron').remote;
     }
   }
 
